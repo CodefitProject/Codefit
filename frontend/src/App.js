140f053853ff011c1f27a3f03c4eba2427a54f48
Kim Jihun <jihun.kim@example.com>
2025-08-11 21:24:57 +0900

    feat : 성향 상세보기 페이지 구현

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage.tsx';
+import MbtiExample from './pages/survey/MbtiExample.js';
 
 function App() {
   return (
     <div className="App">
-      <MainPage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/mbti-example" element={<MbtiExample />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
