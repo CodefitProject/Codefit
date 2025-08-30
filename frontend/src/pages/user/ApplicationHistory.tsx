d284b45f92875e87a83c0e8d3060d5b2686b1f24
Kim Jihun <jihun.kim@example.com>
2025-08-31 03:59:09 +0900

    feat : 유저상세ㅔ화면 구현중

---
+++ frontend/src/pages/user/ApplicationHistory.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ApplicationHistory: React.FC = () => {
+    return (
+        <div>
+            <h2>지원 현황</h2>
+            {/* 추후 이 곳에 지원 현황 목록이 표시됩니다. */}
+        </div>
+    );
+};
+
+export default ApplicationHistory;
---
+++ frontend/src/pages/user/UserProfile.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserProfile: React.FC = () => {
+    return (
+        <div>
+            <h2>프로필 정보</h2>
+            {/* 추후 이 곳에 상세 프로필 정보가 표시됩니다. */}
+        </div>
+    );
+};
+
+export default UserProfile;
